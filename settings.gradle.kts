872bba56b08b5adf799a141aaec91fb1fd0da11d
deagus <dedeaskyt121@gmail.com>
2023-10-24 15:01:04 +0800

    up netflix app to repo

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "netflix-app"
+include(":app")
+ 
